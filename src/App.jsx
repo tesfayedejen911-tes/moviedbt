5cab6c478966a15777a830f07a05b81c71546b04
tesfayedejen911-tes <tesfayedejen911@gmail.com>
2026-06-25 16:49:47 +0300

    latest update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
+
 import "./App.css";
 import Header from "./Components/Header/Header";
 import Banner from "./Components/Banner/Banner";
